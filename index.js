5bcc900c96ff10ef66b1bae25372e8251a4b4de8
kaedenamakura <pvqv315@yahoo.co.jp>
2025-08-15 23:20:17 +0900

    C:/Program Files/Git/やenquetesにアクセスしたときの処理を追加

---
+++ index.js
@@ -0,0 +1,89 @@
+'use strict'
+const http = require('node:http');
+const pug = require('pug');
+
+
+const server = http.createServer((req, res) => {
+  const now = new Date();
+  console.info(`${now} Requested by ${req.socket.remoteAddress}`);
+  res.writeHead(200, {
+    "Content-Type": "text/html; charset=utf-8",
+  });
+
+switch (req.method) {
+  case "GET":
+    if (req.url === '/') {
+      res.write('<!DOCTYPE html><html lang="ja"><body>' +
+        '<h1>アンケートフォーム</h1>' +
+        '<a href="/enquetes">アンケート一覧</a>' +
+        '</body></html>');
+    } else if (req.url === '/enquetes') {
+      res.write('<!DOCTYPE html><html lang="ja"><body>' +
+        '<h1>アンケート一覧</h1><ul>' +
+        '<li><a href="/enquetes/yaki-tofu">焼き肉・湯豆腐</a></li>' +
+        '<li><a href="/enquetes/rice-bread">ごはん・パン</a></li>' +
+        '<li><a href="/enquetes/sushi-pizza">寿司・ピザ</a></li>' +
+        '</ul></body></html>');
+    } else if (req.url === "/enquetes/yaki-tofu") {
+      res.write(pug.renderFile("./form.pug", {
+        path: req.url,
+        firstItem: "焼肉",
+        secondItem: "湯豆腐"
+      }));
+    } else if (req.url === "/enquetes/rice-bread") {
+      res.write(pug.renderFile("./form.pug", {
+        path: req.url,
+        firstItem: "ごはん",
+        secondItem: "パン"
+      }));
+    } else if (req.url === "/enquetes/sushi-pizza") {
+      res.write(pug.renderFile("./form.pug", {
+        path: req.url,
+        firstItem: "寿司",
+        secondItem: "ピザ"
+      }));
+    }
+    res.end();
+    break;
+
+    case "POST":
+      let rawData = "";
+
+      req.on("data", chunk => {
+        rawData += chunk;
+      });
+
+      req.on("end", () => {
+        const answer = new URLSearchParams(rawData);
+
+        // 表示用の文字列を作る
+        const body = `${answer.get("name")}さんは: ${answer.get("favorite")}に投稿しました`;
+
+        console.info(`${now} ${body}`);
+
+        // HTMLレスポンスとして返す
+        res.write(`<html><body><h1>投稿を受け付けました</h1><p>${body}</p></body></html>`);
+        res.end();
+      });
+      break;
+
+    case "DELETE":
+      res.write(`DELETE ${req.url}\n`);
+      res.end();
+      break;
+
+    default:
+      res.write(`Unsupported method: ${req.method}\n`);
+      res.end();
+      break;
+  }
+}).on("error", e => {
+  console.error(`${new Date()} Server Error`, e);
+}).on("clientError", e => {
+  console.error(`${new Date()} Client Error`, e);
+});
+
+const port = 8000;
+server.listen(port, () => {
+  console.log(`${new Date()} Listening on port ${port}`);
+});
